6df2254cb27903c0a109ab73863ff4a6266b323d
SintaYunika <lovelysinta3@gmail.com>
2025-05-21 06:30:34 +0800

    ubah1

--- resources/views/detail/show.blade.php
+++ resources/views/detail/show.blade.php
@@ -4,9 +4,13 @@
 @extends('layouts.app')
 @section('title', $title)
 @section('content')
-    <h1>{{ $article->title }}</h1>
-    <p>{{ $article->description }}</p>
-    <small>Ditulis pada: {{ $article->created_at->format('d M Y') }}</small>
+<div style="display: flex; gap: 30px; align-items: flex-start;">
+    
+    <!-- Kiri: Artikel -->
+    <div style="flex: 2;">
+        <h1>{{ $article->title }}</h1>
+        <p>{{ $article->description }}</p>
+        <small>Ditulis pada: {{ $article->created_at->format('d M Y') }}</small>
     <br><br>
     <a href="{{ route('dashboard') }}">Kembali ke Dashboard</a>
     <a href="{{ route('comments.index') }}">buka Komentar</a>
