1c376b0366f030fd16265f4b521b9eb6b745ea7f
SintaYunika <lovelysinta3@gmail.com>
2025-05-21 05:40:34 +0800

    coba coba

--- resources/views/detail/show.blade.php
+++ resources/views/detail/show.blade.php
@@ -9,4 +9,5 @@
     <small>Ditulis pada: {{ $article->created_at->format('d M Y') }}</small>
     <br><br>
     <a href="{{ route('dashboard') }}">Kembali ke Dashboard</a>
+    <a href="{{ route('comments.index') }}">Lihat Komentar</a>
 @endsection
